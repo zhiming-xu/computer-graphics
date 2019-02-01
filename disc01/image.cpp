579b9aa7231277d0b3bf55531ee1095e7255627c
Zhiming <stevenbirdbrown1998@gmail.com>
2019-01-31 19:57:41 -0800

    fix disc01, the error is about image browser

--- disc01/image.cpp
+++ disc01/image.cpp
@@ -17,6 +17,7 @@ Image::Image(const unsigned& w, const unsigned& h)
 
 void Image::read(std::string filename) {
 	// FIXME
+	data.clear();
 	lodepng::decode(data, width, height, filename);
 }
 
@@ -27,31 +28,38 @@ void Image::write(std::string filename) const {
 
 uint8_t* Image::at(int x, int y) {
 	// FIXME
-	return &data[4*(y*width+x)];
+	return &data[0] + 4*(y*width+x);
 }
 
 Image Image::operator*(const Filter& filter) {
 	// FIXME
 	Image ret=Image(this->width, this->height);
 	// std::cout<<"output image created"<<std::endl;
-	for(int offset=0;offset<4;++offset) {
-		for (int i = 0; i < this->width; ++i) {
-			for (int j = 0; j < this->height; ++j) {
+	for(int offset=0;offset<4;++offset)
+	{
+		for (int i = 0; i < this->width; ++i)
+		{
+			for (int j = 0; j < this->height; ++j)
+			{
 				// printf("%d\t", *this->at(i,j));
 				int w = filter.width, h = filter.height;
 				int shift_i = 0, shift_j = 0;
 				float sum = 0;
 				for (int i_in = 0; i_in < w; ++i_in)
-					for (int j_in = 0; j_in < h; ++j_in) {
+				{
+					for (int j_in = 0; j_in < h; ++j_in)
+					{
 						// std::cout<<"before this->at"<<std::endl;
 						shift_i = i - w / 2 + i_in;
 						shift_j = j - h / 2 + j_in;
-						if (shift_i >= 0 && shift_i < width && shift_j >= 0 && shift_j < height) {
-							int r = *(this->at(shift_i, shift_j)+offset);
+						if (shift_i >= 0 && shift_i < width && shift_j >= 0 && shift_j < height)
+						{
+							int r = *(this->at(shift_i, shift_j) + offset);
 							sum += filter.at(i_in, j_in) * r;
 						}
 					}
-				*(ret.at(i, j)+offset) = (uint8_t)sum;
+				}
+				ret.at(i, j)[offset] = (uint8_t)sum;
 			}
 		}
 	}
